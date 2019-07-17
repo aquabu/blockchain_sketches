0670c24bbced9f4c9e6f319f88e668b4a3324a25
Noah Thorp <noah@comakery.com>
2019-07-16 17:56:42 -0700

    cleanup comments and semicolons in server.js

--- server.js
+++ server.js
@@ -1,24 +1,12 @@
 var fs = require('fs');
 var http = require('http');
 
-// http.createServer(function (request, response) {
-//     response.writeHead(200, {'Content-Type': 'text/html'})
-//     fs.readFile('index.html', function(err, content) {
-//         response.write(content)
-//         response.end()
-//     })
-// }).listen(8080)
-
-http.createServer(function(request, response) {
-    response.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-
-    fs.readFile('index2.html', function(err, content){
-        response.write(content);
-        response.end();
-    });
-
-}).listen(8080);
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/html'})
+    fs.readFile('index2.html', function(err, content) {
+        response.write(content)
+        response.end()
+    })
+}).listen(8080)
 
 console.log('Listening on port 8080');
