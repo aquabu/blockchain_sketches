40d111ea2658f3c55a3f55d9c3a4ff6fb989ff97
Noah Thorp <noah@comakery.com>
2019-08-05 22:29:36 -0700

    add dynamic index of static files

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const app = express()
 const router = express.Router()
+const path = require('path')
+const fs = require('fs')
+const root = path.join(__dirname, '/static')
 
-app.use(express.static(__dirname + '/static'))
-app.use('/', router)
+app.get('/', (req, res) => {
+    fs.readdir(root, (err, files) => {
+        res.write(
+            "<!doctype html><html lang='en'><title>Index</title><ul>"
+        )
+        files.forEach((file) => {
+            res.write(`<li><a href='${file}'>${ file.replace( /\..*$/, '' ) }</a></li>`)
+        })
+        res.write('</ul>')
+        res.end()
+    })
+})
+app.use(express.static('static'))
 app.listen(8080)
 
 console.log('Listening on port 8080')
