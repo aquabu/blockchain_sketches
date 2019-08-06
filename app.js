d785e67d74160aa4d885d9d094bfe41512766ebc
Noah Thorp <noah@comakery.com>
2019-08-05 21:03:57 -0700

    remove commented out code

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const express = require('express')
 const app = express()
 const router = express.Router()
 
-// app.get('/', function (req, res) {
-//     res.sendFile(path.join(__dirname, '/express/index.html'))
-// })
 app.use(express.static(__dirname + '/static'))
 app.use('/', router)
 app.listen(8080)
